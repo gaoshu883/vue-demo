96fceb284e01196686dd16a5546688a345bd3cf3
wangxiaolu <gaoshu883@163.com>
2019-03-31 14:37:27 +0800

    添加新文件

---
+++ src/assets/js/data.js
@@ -0,0 +1,13 @@
+let demo = `      
+<template>
+  <div>Hello, {{ name }}!</div>
+</template>
+
+<script>
+  module.exports = {
+    data: function () {
+      return { name: 'Vue' }
+    }
+  }
+</script>`
+export default demo
